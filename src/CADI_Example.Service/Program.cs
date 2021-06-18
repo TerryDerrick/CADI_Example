9d97379562a27e2c6c4eddd6a3332ee4f1abed4e
Terry Derrick <lord_silver_fox@hotmail.com>
2021-06-18 17:28:23 +1000

    updates

---
+++ src/CADI_Example.Service/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CADI_Example.Service
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
