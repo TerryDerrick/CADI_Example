74fd6b884c92dca4c0e7019caf58c45700156c31
Terry Derrick <lord_silver_fox@hotmail.com>
2021-06-21 19:30:51 +1000

    Added base Persistance

---
+++ src/CADI_Example.Persistance/ServiceCollectionExtensions.cs
@@ -0,0 +1,39 @@
+﻿namespace CADI_Example.Persistence
+{
+    using System;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+
+    /// <summary>
+    /// Handles contextual Depenency Injection.
+    /// </summary>
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Connection String name expected in appsettings.json when not otherwise specified.
+        /// </summary>
+        public const string DefaultConnectionStringName = "DatabaseConnectionString";
+
+        /// <summary>
+        /// Adds services to <see cref="IServiceCollection"/> relevant to the Persistance Layer.
+        /// </summary>
+        /// <param name="services">Service Collection.</param>
+        /// <param name="configuration">Configuration.</param>
+        /// <param name="connectionStringName">ConnectionString name in application settings.</param>
+        /// <returns>Updated Service Collection.</returns>
+        public static IServiceCollection AddPersistanceServices(this IServiceCollection services, IConfiguration configuration, string connectionStringName = DefaultConnectionStringName)
+        {
+            if (configuration is null)
+            {
+                throw new ArgumentNullException(nameof(configuration));
+            }
+
+            //services.AddDbContext<SystemDbContext>(options => options.UseSqlServer(configuration.GetConnectionString(connectionStringName)));
+
+            //services.AddScoped<ISystemDbContext>(provider => provider.GetService<SystemDbContext>());
+
+            return services;
+        }
+    }
+}
